fefced03838049d1dc269b4103df91dbf7205edb
wucong-github <3288448426@qq.com>
2020-03-22 08:34:19 +0800

    提交microservicecloud 项目完整版，修改测试

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 // 本服务启动后会作为eureka的客户端，因为application没有禁止，所以会自动注册进eureka 注册服务中心
 @EnableEurekaClient
-// 服务发现，让其他服务能够发现本服务
+// 服务发现，让其他服务能够发现本服务，增加注解修改
 @EnableDiscoveryClient
 public class DeptProvider8001_App {
 
